acacbc4f965311df3ffffc3a200d937adc02efdc
wangxuezhong <wxzxingtian@163.com>
2023-01-29 15:13:57 +0800

    调试

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    domains: ['upload.wikimedia.org'],
+    // loader: 'imgix',
+  },
+};
 
 module.exports = nextConfig
